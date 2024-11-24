76ad5d4d9043a4ea1351fb97c5b00fcabd49d53a
Guilhermefariah <Guilhermefaria321@gmail.com>
2024-11-24 18:21:25 -0300

    fix: Estrutura inicial para injeção de repositorio e metodo Get

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"go-api/controller"
+	"go-api/usecase"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	server := gin.Default()
 
-	server.GET("/ping", func(ctx *gin.Context){
+	ProductUseCase := usecase.NewProductUseCase()
+	//Controllers
+	ProductController := controller.NewProductController(ProductUseCase)
+
+	server.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "ping",
-
 		})
 	})
+
+	server.GET("/products", ProductController.GetProducts)
+
 	server.Run(":8000")
 }
--- controller/product_controller.go
+++ controller/product_controller.go
@@ -1,25 +1,30 @@
-package controller 
+package controller
 
 import (
 	"go-api/model"
+	"go-api/usecase"
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
+
 type ProductController struct {
-	//Usecase
+	ProductUseCase usecase.ProductUseCase
 }
 
-func NewProductController() ProductController {
-	return ProductController{}
+func NewProductController(usecase usecase.ProductUseCase) ProductController {
+	return ProductController{
+		ProductUseCase: usecase,
+	}
 }
 
 func (p *ProductController) GetProducts(ctx *gin.Context) {
 	products := []model.Product{
 		{
-			ID: 1,
-			Name: "Coca-Cola",
+			ID:    1,
+			Name:  "Coca-Cola",
 			Price: 20,
-		}
+		},
 	}
 	ctx.JSON(http.StatusOK, products)
 }
