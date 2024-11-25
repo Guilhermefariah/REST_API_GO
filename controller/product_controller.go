d3054cc55c96376055217551a5581d14e8e77b43
Guilhermefariah <Guilhermefaria321@gmail.com>
2024-11-25 08:15:55 -0300

    feat: Adicionando metodo GetProduct em ProductController

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -4,6 +4,7 @@ import (
 	"go-api/model"
 	"go-api/usecase"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -44,4 +45,31 @@ func (p *ProductController) CreateProduct(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusCreated, insertedProduct)
+}
+
+func (p *ProductController) GetProductById(ctx *gin.Context) {
+	id := ctx.Param("producId")
+	if id == "" {
+		response := model.Response{
+			Message: "Product ID cannot be null",
+		}
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	productId, err := strconv.Atoi(id)
+	if err != nil {
+		response := model.Response{
+			Message: "Product ID must be a number",
+		}
+		ctx.JSON(http.StatusInternalServerError, response)
+		return
+	}
+
+	products, err := p.ProductUseCase.GetProductById(productId)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+	}
+
+	ctx.JSON(http.StatusOK, products)
 }
